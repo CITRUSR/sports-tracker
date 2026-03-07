e71de31c780cac49844c07868265c3234be7d8a5
CITRUSR <savelypetrov2007@mail.ru>
2026-03-07 21:14:25 +0300

    test ci-cd commit

--- front/src/features/home/HomePage.jsx
+++ front/src/features/home/HomePage.jsx
@@ -7,7 +7,7 @@ function HomePage() {
     api.healthCheck().then((response) => console.log(response));
   }, []);
 
-  return <div className={styles.container}>Home</div>;
+  return <div className={styles.container}>Home test ci-cd</div>;
 }
 
 export default HomePage;
