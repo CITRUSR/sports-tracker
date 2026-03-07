d099650cb082597f846b34853e33ccd524ddce6f
CITRUSR <savelypetrov2007@mail.ru>
2026-03-07 22:12:12 +0300

    test ci-cd

--- front/src/features/home/HomePage.jsx
+++ front/src/features/home/HomePage.jsx
@@ -1,13 +1,22 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import styles from './HomePage.module.css';
 import api from '../api/api';
 
 function HomePage() {
+  const [backVersion, setBackVersion] = useState();
+
   useEffect(() => {
-    api.healthCheck().then((response) => console.log(response));
+    api.healthCheck().then((response) => {
+      console.log(response);
+      setBackVersion(response);
+    });
   }, []);
 
-  return <div className={styles.container}>Home test ci-cd</div>;
+  return (
+    <div className={styles.container}>
+      Home test ci-cd back version: {backVersion}
+    </div>
+  );
 }
 
 export default HomePage;
