37e0133204588520ee02945a56c7efca82a03342
CITRUSR <savelypetrov2007@mail.ru>
2026-03-11 23:56:35 +0300

    feat(Auth): add register endpoint

---
+++ back/Features/Auth/AuthEndpoints.cs
@@ -0,0 +1,33 @@
+using System.ComponentModel.DataAnnotations;
+using back.Common.Markers;
+
+namespace back.Features.Auth;
+
+public class AuthEndpoints : IEndpointMarker
+{
+    private const string _baseRoute = "auth";
+
+    public void MapEndpoints(RouteGroupBuilder app)
+    {
+        app.MapPost($"{_baseRoute}/register", async (RegisterUserDto dto, IAuthService authService) =>
+        {
+            var context = new ValidationContext(dto);
+            var validationResults = new List<ValidationResult>();
+            if (!Validator.TryValidateObject(dto, context, validationResults, true))
+                return Results.BadRequest(validationResults.Select(x => x.ErrorMessage));
+
+            var result = await authService.RegisterUserAsync(dto);
+            if (!result.IsSuccess)
+                return Results.BadRequest(result.Errors);
+
+            return Results.Ok();
+        })
+        .WithDescription(
+            "Password must be at least 6 characters long, contain at least one uppercase letter, " +
+            "one lowercase letter, one number and one special character.\n" +
+            "Login must be unique.\n\n"
+        )
+        .Produces(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status400BadRequest, typeof(IEnumerable<string>));
+    }
+}
