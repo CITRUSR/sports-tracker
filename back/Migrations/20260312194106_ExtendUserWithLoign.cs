f2fc3400a377cc9a2c560acc127df652c0282a3b
CITRUSR <savelypetrov2007@mail.ru>
2026-03-12 22:41:27 +0300

    feat(db): extend user with login column

---
+++ back/Migrations/20260312194106_ExtendUserWithLoign.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace back.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendUserWithLoign : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Login",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_Login",
+                table: "AspNetUsers",
+                column: "Login",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_Login",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "Login",
+                table: "AspNetUsers");
+        }
+    }
+}
