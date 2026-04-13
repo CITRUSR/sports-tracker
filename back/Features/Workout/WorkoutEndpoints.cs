bc0ebcc329ceca41107caae6651f90769c700d54
CITRUSR <savelypetrov2007@mail.ru>
2026-04-13 10:38:51 +0300

    feat(Workout): add begin endpoint

---
+++ back/Features/Workout/WorkoutEndpoints.cs
@@ -0,0 +1,30 @@
+using back.Common.Extensions;
+using back.Common.Markers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace back.Features.Workout;
+
+public class WorkoutEndpoints : IEndpointMarker
+{
+    private const string _baseRoute = "workouts";
+    private const string _tag = "Workout";
+
+    public void MapEndpoints(RouteGroupBuilder app)
+    {
+        app.MapPost(_baseRoute, async ([FromServices] IWorkoutService workoutService, HttpContext context) =>
+        {
+            var userId = context.User.GetId();
+
+            var result = await workoutService.BeginAsync(userId);
+            if (!result.IsSuccess)
+                return Results.Conflict(result.ErrorsString);
+
+            return Results.Ok();
+        })
+        .RequireAuthorization()
+        .WithTags(_tag)
+        .WithDescription("Begin workout 1 user can have only 1 active workout")
+        .Produces(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status409Conflict, typeof(string));
+    }
+}
