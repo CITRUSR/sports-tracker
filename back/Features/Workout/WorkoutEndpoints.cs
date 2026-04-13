f7c3b931bc9fe61ae5f53867ce8bc983d60326aa
CITRUSR <savelypetrov2007@mail.ru>
2026-04-13 10:43:39 +0300

    feat(Workout): add finish endpoint

--- back/Features/Workout/WorkoutEndpoints.cs
+++ back/Features/Workout/WorkoutEndpoints.cs
@@ -26,5 +26,23 @@ public class WorkoutEndpoints : IEndpointMarker
         .WithDescription("Begin workout 1 user can have only 1 active workout")
         .Produces(StatusCodes.Status200OK)
         .Produces(StatusCodes.Status409Conflict, typeof(string));
+
+        // rpc style because command
+        app.MapPost($"{_baseRoute}/finish", async ([FromServices] IWorkoutService workoutService, [FromBody] string comment,
+            HttpContext context) =>
+        {
+            var userId = context.User.GetId();
+
+            var result = await workoutService.FinishAsync(userId, comment);
+            if (!result.IsSuccess)
+                return Results.NotFound(result.ErrorsString);
+
+            return Results.Ok();
+        })
+        .RequireAuthorization()
+        .WithTags(_tag)
+        .WithDescription("Finish workout")
+        .Produces(StatusCodes.Status200OK)
+        .Produces(StatusCodes.Status404NotFound, typeof(string));
     }
 }
