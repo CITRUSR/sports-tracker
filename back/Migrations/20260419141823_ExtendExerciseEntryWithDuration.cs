f00491e1a796d0561b1b7561fbfe2225b3a9defa
CITRUSR <savelypetrov2007@mail.ru>
2026-04-19 17:19:15 +0300

    feat(db): extend ExerciseEntry with duration

---
+++ back/Migrations/20260419141823_ExtendExerciseEntryWithDuration.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace back.Migrations
+{
+    /// <inheritdoc />
+    public partial class ExtendExerciseEntryWithDuration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<TimeSpan>(
+                name: "Duration",
+                table: "ExerciseEntries",
+                type: "interval",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Duration",
+                table: "ExerciseEntries");
+        }
+    }
+}
