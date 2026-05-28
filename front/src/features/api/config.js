ff437594b4743db6556b0bc1645ec69612d3e6f8
CITRUSR <savelypetrov2007@mail.ru>
2026-05-28 17:35:52 +0300

    feat(front): update back url

--- front/src/features/api/config.js
+++ front/src/features/api/config.js
@@ -3,5 +3,5 @@ const useLocalEndpoints = false;
 export const urls = {
   ...(useLocalEndpoints
     ? { api: 'http://localhost:5000/api' }
-    : { api: 'http://176.108.255.65:80/api' }),
+    : { api: 'https://sports-tracker.ru/api' }),
 };
